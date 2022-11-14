1b29ad54545cca6fc11dec458d037b4f1627395a
e.dyakov <e3001dk@gmail.com>
2022-11-14 23:43:15 +0300

    написал функции по генерации случайных чисел, целого и float

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-тестовое содержимое
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomIntInclusive(1, 3));
+
+function getRandomFloatInclusive(min, max, floatValue) {
+  const value = Math.random() * (max - min) + min;
+  return value.toFixed(floatValue);
+}
+
+console.log(getRandomFloatInclusive(0.1, 0.3, 3));
