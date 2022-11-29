16884b99c0d61633552ebd284234c09cee4efd42
e.dyakov <e3001dk@gmail.com>
2022-11-30 01:03:23 +0300

    добавляет ф-ии по генерации перемешанного числового массива и массива из перемешанных урлов для объекта author

--- js/main.js
+++ js/main.js
@@ -1,13 +1,52 @@
-function getRandomIntInclusive (min, max) {
+function getRandomInt (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomIntInclusive(1, 3);
-
-function getRandomFloatInclusive (min, max, float) {
+function getRandomFloat (min, max, float) {
   const value = Math.random() * (max - min) + min;
   return value.toFixed(float);
 }
 
-getRandomFloatInclusive(0.25, 0.26, 2);
+function getMixedNumberArray (length) {
+  const array = [];
+  for (let i = 1; i <= length; i++) {
+    let number = getRandomInt(1, length);
+    while(array.includes(number)) {
+      number = getRandomInt(1, length);
+    }
+    array.push(number);
+  }
+  return array;
+}
 
+function getAvatarsUrls (quantity) {
+  const array = getMixedNumberArray(quantity);
+  return array.map((element) => element < 10 ? `img/avatars/user0${element}.png` : `img/avatars/user${element}.png`);
+}
+
+// const avatarsUrls = getAvatarsUrls(4);
+
+// function getAvatar () {
+//   return {
+//     avatar:
+//   }
+// }
+
+
+// создать массив из четырех объектов с перемешанными адресами
+
+// const result = Array.from({length: 4}, );
+
+// const someObj = {
+//   author: {
+//     avatar: 'адрес изображения'
+//   },
+//   offer: {
+//     title: 'заголовок объявления',
+//     address: 'адрес',
+//     price: 50000,
+//     type: '',
+//     rooms: 3,
+
+//   },
+// };
